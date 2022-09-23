d5235cce8f1a15fafb348aeb182b769bcce8d05c
Roman Kamenivskyi <rk@wwwest.solutions>
2022-09-23 20:38:12 +0300

    add missing link

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,4 +2,5 @@ export const pages = [
   { id: 1, to: "/", label: "Charachters" },
   { id: 2, to: "/episodes", label: "Episodes" },
   { id: 3, to: "/locations", label: "Locations" },
+  { id: 3, to: "/watch-list", label: "Watch list" },
 ];
