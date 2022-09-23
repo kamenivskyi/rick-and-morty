c63cf20290a4a99789da851749fee52be7ef11cc
Roman Kamenivskyi <rk@wwwest.solutions>
2022-09-23 21:43:24 +0300

    change browser roter to hash, change link manifest

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
