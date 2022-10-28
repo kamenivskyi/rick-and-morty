a826bee20c5657d6b038439b6289dc9ebca81de1
Roman Kamenivskyi <rk@wwwest.solutions>
2022-10-28 22:44:07 +0300

    delete console log

--- src/features/charachters/charachtersAPI.ts
+++ src/features/charachters/charachtersAPI.ts
@@ -4,6 +4,5 @@ export async function fetchCharachters<T>(stringifiedParams: string) {
   const url = `/character${stringifiedParams}`;
   const request = await fetchData<T>(url);
 
-  console.log("resp: ", request);
   return request;
 }
